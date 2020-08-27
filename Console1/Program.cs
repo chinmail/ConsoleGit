df99a3b05982a06dc5ec2edab161d78292d353ae
Kosta <chinmail@mail.ru>
2020-08-27 16:36:59 +0300

    Hello World

--- Console1/Program.cs
+++ Console1/Program.cs
@@ -10,6 +10,10 @@ namespace Console1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wello World!");
+
+            // Delay
+            Console.ReadKey(true);
         }
     }
 }
